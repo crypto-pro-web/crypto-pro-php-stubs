e3b57cd332737866c2c9d493952fbc0b54d12cff
Artem Vasilev <kern.usr@gmail.com>
2022-10-24 16:37:30 +0300

    Описание класса коллекции объектов CPAttribute.

--- src/CPAttributes.php
+++ src/CPAttributes.php
@@ -1,20 +1,64 @@
 <?php
 
+/**
+ * Коллекция объектов CPAttribute.
+ *
+ * @property-read int $Count
+ * @property-read int $Item
+ */
 class CPAttributes
 {
-	
-	public function __construct(){}
-	
-	public function Add(){}
-	
-	public function get_Count(){}
-	
-	public function get_Item(){}
-	
-	public function Clear(){}
-	
-	public function Remove(){}
-	
-	public function Assign(){}
-	
+	/**
+	 * Добавляет атрибут в коллекцию.
+	 *
+	 * @param   CPAttribute  $attribute
+	 *
+	 * @return void
+	 */
+	public function Add(CPAttribute $attribute)
+	{
+	}
+
+	/**
+	 * Возвращает количество атрибутов в коллекции.
+	 *
+	 * @return int
+	 */
+	public function get_Count()
+	{
+	}
+
+	/**
+	 * Возвращает атрибут из коллекции по его индексу.
+	 *
+	 * @return CPAttribute
+	 */
+	public function get_Item(int $index)
+	{
+	}
+
+	/**
+	 * Удаляет все атрибуты из коллекции.
+	 *
+	 * @return void
+	 */
+	public function Clear()
+	{
+	}
+
+	/**
+	 * Удаляет атрибут из коллекции.
+	 *
+	 * @param   int  $index
+	 *
+	 * @return void
+	 */
+	public function Remove(int $index)
+	{
+	}
+
+	public function Assign()
+	{
+	}
+
 }
