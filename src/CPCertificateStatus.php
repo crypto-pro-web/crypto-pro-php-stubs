073e61fa6f52c10678dbd247b9fe25cd658593a5
Artem Vasilev <kern.usr@gmail.com>
2022-11-03 17:58:11 +0300

    Заполнение докблока у вызываемых методов

--- src/CPCertificateStatus.php
+++ src/CPCertificateStatus.php
@@ -2,29 +2,32 @@
 
 class CPCertificateStatus
 {
-	
+
 	public function __construct(){}
-	
+
+	/**
+	 * @return bool
+	 */
 	public function get_Result(){}
-	
+
 	public function get_CheckFlag(){}
-	
+
 	public function set_CheckFlag(){}
-	
+
 	public function EKU(){}
-	
+
 	public function get_VerificationTime(){}
-	
+
 	public function set_VerificationTime(){}
-	
+
 	public function get_UrlRetrievalTimeout(){}
-	
+
 	public function set_UrlRetrievalTimeout(){}
-	
+
 	public function CertificatePolicies(){}
-	
+
 	public function ApplicationPolicies(){}
-	
+
 	public function get_ValidationCertificates(){}
-	
+
 }
