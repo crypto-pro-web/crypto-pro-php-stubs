0bf897d4a4d9b092999aa310e8ab2b8d26acd530
Artem Vasilev <kern.usr@gmail.com>
2022-11-15 12:19:03 +0300

    Описание к классу About

--- src/About.php
+++ src/About.php
@@ -1,20 +1,70 @@
 <?php
 
+/**
+ * Версия библиотеки.
+ */
 class About
 {
-	
-	public function __construct(){}
-	
-	public function get_MajorVersion(){}
-	
-	public function get_MinorVersion(){}
-	
-	public function get_BuildVersion(){}
-	
-	public function get_Version(){}
-	
-	public function CSPVersion(){}
-	
-	public function PluginVersion(){}
-	
+
+	/**
+	 *
+	 */
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Возвращает старший компонент версии.
+	 *
+	 * @return int
+	 */
+	public function get_MajorVersion()
+	{
+	}
+
+	/**
+	 * Возвращает младший компонент версии.
+	 *
+	 * @return int
+	 */
+	public function get_MinorVersion()
+	{
+	}
+
+	/**
+	 * Возвращает номер сборки.
+	 *
+	 * @return int
+	 */
+	public function get_BuildVersion()
+	{
+	}
+
+	/**
+	 * Возвращает строковое представление версии.
+	 *
+	 * @return string
+	 */
+	public function get_Version()
+	{
+	}
+
+	/**
+	 * Возвращает объект \Version c версией криптопровайдера (CSP).
+	 *
+	 * @return \Version
+	 */
+	public function CSPVersion()
+	{
+	}
+
+	/**
+	 * Возвращает объект \Version c версией плагина.
+	 *
+	 * @return \Version
+	 */
+	public function PluginVersion()
+	{
+	}
+
 }
